5949a1ce769c5727a2273f5738a88200914cd101
javiersaezgarcia <banbleac@gmail.com>
2023-04-11 17:58:12 +0200

    second

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { leerInput, inquirerMenu, pause } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 
 
@@ -7,19 +8,37 @@ const { leerInput, inquirerMenu, pause } = require("./helpers/inquirer");
 const main = async() => {
     // const texto = await leerInput('Hola: ');
     let opt;
+
+    const busquedas = new Busquedas();
    
     do{
         opt = await inquirerMenu();
         switch (opt) {
-            case 1:              
+            case 1:    
+              // Mostrar mensaje          
               const ciudad = await leerInput('Ciudad:');
-              console.log(ciudad);             
+              console.log(ciudad);   
+              
+              // Mostrar los lugares
+
+              // Seleccionar el lugar
+
+              //  clima
+
+              // Mostrar resultados
+              console.log('\nInformación de la ciudad\n');
+              console.log('Ciudad: ');
+              console.log('Lat: ');
+              console.log('Lng: ');
+              console.log('Temperatura: ');
+              console.log('Mínima: ');
+              console.log('Máxima: ');
               break;
             case 2:
-              await console.log('hola 2');
+             // sdfgsdf
               break;
             case 0:
-              await console.log('salgo');
+              console.log('salgo');
               break;
 
         }
---
+++ models/busquedas.js
@@ -0,0 +1,19 @@
+
+
+class Busquedas {
+
+    historial = ['Madrid', 'Barcelona', 'Sagunto'];
+
+    constructor() {
+        // leer DB si existe
+    }
+
+    async ciudad( lugar = '') {
+        console.log(lugar);
+
+        return []; // retornar lugares que  coincidan con la busqueda
+    }
+
+}
+
+module.exports = Busquedas;
